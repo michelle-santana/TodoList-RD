f7e9d9157884384b250c4f3566edca595af2e825
michelle-santana <msjesus@rd.com.br>
2020-02-06 08:46:58 -0300

    Atualizando projeto

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-let btnAddTarefa = document.getElementById("imagem")
+let btnAddTarefa = document.getElementById("imagem") //função responsável pelo funcionamento do botão//
 let tarefas = document.querySelector("#tarefas")
 let inputTarefa = document.querySelector("#tarefa-digitada")
 
 console.log(btnAddTarefa);
 
-btnAddTarefa.onclick = function(){
+btnAddTarefa.onclick = function(){ //definição de clique//
 
     let valorDigitado = inputTarefa.value
 
-    //alert("Botão Clicado!")
+    //alert("Botão Clicado!") Essa função dispara um alerta na tela quando o botão é clicado.//
  
     let tarefaNova = `<div class="col-lg-4">
     <div class="card-tarefa">
         <div class="texto Tarefa">
             Adcione uma tarefa
-            ${valorDigitado}
+            ${valorDigitado} 
         </div>
         <div class="botão-tarefa">
             <img src="../imagens/botão check.png" alt="Botão check" title="Botão check" width="50px">
