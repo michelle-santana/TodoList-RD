2f18590a1474587f51a58755862b590632c66077
michelle-santana <msjesus@rd.com.br>
2020-02-07 08:10:18 -0300

    projeto finalizado

--- js/script.js
+++ js/script.js
@@ -1,42 +1,58 @@
-let btnAddTarefa = document.getElementById("imagem") //função responsável pelo funcionamento do botão//
+let btnAddTarefa = document.getElementById("imagem") //função adcionar barra de tarefa//
 let tarefas = document.querySelector("#tarefas")
-let inputTarefa = document.querySelector("#tarefa-digitada") 
+let inputTarefa = document.querySelector("#tarefa-digitada")
+let listaTarefas = localStorage.getItem("listaTarefas") ? JSON.parse(localStorage.getItem("listaTarefas")) : []
 
-console.log(btnAddTarefa);
+const salvarLocalStorage = tarefas => {
+    let tarefasEmJson = JSON.stringify(tarefas)
+    localStorage.setItem("listaTarefas", tarefasEmJson)
+    console.log("tarefa salva com sucesso!");
+}
 
-//btnAddTarefa.onclick = function () { //definição de clique//
+const mostrarNaTela = arrayTarefas => {
+    arrayTarefas.forEach(textoTarefa => {
+        let tarefaNova = `<div class="col-lg-4">
+        <div class="card-tarefa">
+            <div class="texto Tarefa">
+                ${textoTarefa} 
+            </div>
+            <div class="botão-tarefa">
+                <img src="../imagens/botão check.png" alt="Botão check" title="Botão check" width="50px">
+            </div>
+        </div>
+    </div>`
 
-    //let valorDigitado = inputTarefa.value //pega os valores digitados//
-    //if (valorDigitado == "") {
-       // alert("Você deve digitar uma tarefa primeiro.") //Essa função dispara um alerta na tela quando o botão é clicado.//
-       // return
-    //}
+        tarefas.insertAdjacentHTML("beforeend", tarefaNova)
 
 
-   // let tarefaNova = `<div class="col-lg-4">
-   // <div class="card-tarefa">
-       // <div class="texto Tarefa">
-         //   ${valorDigitado} 
-       // </div>
-       // <div class="botão-tarefa">
-         //   <img src="../imagens/botão check.png" alt="Botão check" title="Botão check" width="50px">
-      //  </div>
-    //</div>
-//</div>`
 
-   // tarefas.innerHTML += tarefaNova
+        let objTarefaNova = tarefas.lastElementChild
+        let btnCheckTarefaNova = objTarefaNova.lastElementChild.lastElementChild
+        btnCheckTarefaNova.onclick = (event) => {
 
-//}//
+            tarefas.removeChild(event.target.parentNode.parentNode.parentNode)
+            listaTarefas = listaTarefas.filter(valor => valor != textoTarefa)
 
-const criarTarefaComEnter = (event) => {
-    
+            salvarLocalStorage(listaTarefas)
+
+        }
+
+    })
+
+}
+
+mostrarNaTela(listaTarefas)
+
+const criarTarefaComEnter = event => {
+
     if (event.keyCode == 13 || event.type == "click") {
 
-        let valorDigitado = inputTarefa.value
+        let valorDigitado = inputTarefa.value //quando usuário insere um valor//
         if (valorDigitado == "") {
             alert("Você deve digitar uma tarefa primeiro.") //Essa função dispara um alerta na tela quando o botão é clicado.//
             return
         }
+
 
         inputTarefa.value = ""
         let tarefaNova = `<div class="col-lg-4">
@@ -50,19 +66,26 @@ const criarTarefaComEnter = (event) => {
     </div>
 </div>`
 
-        tarefas.insertAdjacentHTML("beforeend",tarefaNova) //funcionalidade clicar no check//
+        listaTarefas.push(valorDigitado); //capta o valor digitado na tarefa//
+        salvarLocalStorage(listaTarefas)
+
+
+        tarefas.insertAdjacentHTML("beforeend", tarefaNova) //funcionalidade clicar no check//
 
         let objTarefaNova = tarefas.lastElementChild
         let btnCheckTarefaNova = objTarefaNova.lastElementChild.lastElementChild
-        objTarefaNova.onclick=(event) =>
+        objTarefaNova.onclick = (event) => {
+            tarefas.removeChild(event.target.parentNode.parentNode.parentNode)
+        }
 
-        console.log(event.target.parentNode.parentNode.parentNode.remove())
 
     }
-    
+
 }
 
 inputTarefa.addEventListener("keypress", criarTarefaComEnter)
 btnAddTarefa.addEventListener("click", criarTarefaComEnter)
+
+
 
 
